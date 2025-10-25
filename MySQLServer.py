58d80f668af8f3133958b169e5a663b3299f23e4
wilkens001 <mbobwilkens@gmail.com>
2025-10-25 20:33:53 +0200

    Remove is_connected() calls to avoid SELECT/SHOW statements

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,22 +17,21 @@ def create_database():
             password=''  # Update with your MySQL password
         )
         
-        if connection.is_connected():
-            cursor = connection.cursor()
-            
-            # Create database if not exists
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            
-            print("Database 'alx_book_store' created successfully!")
-            
-            cursor.close()
+        cursor = connection.cursor()
+        
+        # Create database if not exists
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        
+        print("Database 'alx_book_store' created successfully!")
+        
+        cursor.close()
             
     except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
         
     finally:
         # Close the connection
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             connection.close()
 
 
